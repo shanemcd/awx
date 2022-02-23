48b202bb838d5fbe2c1c19ced894e4f355b7c67a
Marliana Lara <mlara@redhat.com>
2022-02-23 14:27:50 -0500

    Add unique row id to subscription modal list items

--- awx/ui/src/screens/Setting/Subscription/SubscriptionEdit/SubscriptionStep.js
+++ awx/ui/src/screens/Setting/Subscription/SubscriptionEdit/SubscriptionStep.js
@@ -229,7 +229,7 @@ function SubscriptionStep() {
                   username: username.value,
                   password: password.value,
                 }}
-                selectedSubscripion={subscription?.value}
+                selectedSubscription={subscription?.value}
                 onClose={closeModal}
                 onConfirm={(value) => subscriptionHelpers.setValue(value)}
               />
