e0d187818eedeaa1350b0b76145a95a35e0bdafb
Shane McDonald <me@shanemcd.com>
2022-02-24 17:15:47 -0500

    Move DEFAULT_CONTAINER_VOLUME_MOUNTS into production.py
    This doesnt work in the development environment.

--- awx/settings/defaults.py
+++ awx/settings/defaults.py
@@ -992,7 +992,4 @@ DEFAULT_CONTAINER_RUN_OPTIONS = []
 # List of default exposed container volume mounts
 # This is useful when the EE container needs to access host/node information
 # like /etc/pki/ca-trust/source/anchors
-DEFAULT_CONTAINER_VOLUME_MOUNTS = [
-    '/etc/pki/ca-trust:/etc/pki/ca-trust:ro',
-    '/usr/share/pki:/usr/share/pki:ro',
-]
+DEFAULT_CONTAINER_VOLUME_MOUNTS = []
