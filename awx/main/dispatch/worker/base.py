5f2c9fb604635f2c3523c127d57aa6dcfb221790
Alan Rominger <arominge@redhat.com>
2022-07-13 09:48:44 -0400

    Bump timer

--- awx/main/dispatch/worker/base.py
+++ awx/main/dispatch/worker/base.py
@@ -104,7 +104,7 @@ class AWXConsumerBase(object):
         self.record_statistics()
         if isinstance(body, dict) and 'time_pub' in body:
             tq = time.time()
-            if tq - body['time_pub'] > 0.01:
+            if tq - body['time_pub'] > 0.05:
                 logger.warning(
                     f'Dispatching task took {body.get("task")} too long: notify {body["time_ack"] - body["time_pub"]:.4f} queued {tq - body["time_ack"]:.4f}'
                 )
